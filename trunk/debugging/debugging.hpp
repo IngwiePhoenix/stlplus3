cf4dbd56efbe80924501dfb499c55464244a5f20
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2007-08-10 13:49:48 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@16 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

---
+++ trunk/debugging/debugging.hpp
@@ -0,0 +1,16 @@
+#ifndef DEBUGGING_HPP
+#define DEBUGGING_HPP
+/*------------------------------------------------------------------------------
+
+  Author:    Andy Rushton
+  Copyright: (c) Andy Rushton, 2004
+  License:   BSD License, see ../docs/license.html
+
+  Include all debugging utilities in one go.
+
+------------------------------------------------------------------------------*/
+
+#include "debug.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
