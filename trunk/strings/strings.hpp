06a7a192efe08e010d5409512b5803acee2b6a50
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2007-09-08 16:03:18 +0000

    Header to include whole library in one go
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@25 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

---
+++ trunk/strings/strings.hpp
@@ -0,0 +1,21 @@
+#ifndef STRINGS_HPP
+#define STRINGS_HPP
+/*------------------------------------------------------------------------------
+
+Author:    Andy Rushton
+Copyright: (c) Andy Rushton, 2004
+License:   BSD License, see ../docs/license.html
+
+Header for including the whole strings library in one go
+
+------------------------------------------------------------------------------*/
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "format_types.hpp"
+#include "strings_basic.hpp"
+#include "strings_stl.hpp"
+#include "strings_utilities.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
