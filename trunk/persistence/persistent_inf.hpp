8364857111888b8688458a6197ed0aefff826f58
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2007-10-18 17:33:46 +0000

    Persistence functions for the stlplus infinite precision integer type
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@41 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

---
+++ trunk/persistence/persistent_inf.hpp
@@ -0,0 +1,23 @@
+#ifndef STLPLUS_PERSISTENT_INF_HPP
+#define STLPLUS_PERSISTENT_INF_HPP
+/*------------------------------------------------------------------------------
+
+  Author:    Andy Rushton
+  Copyright: (c) Andy Rushton, 2007
+  License:   BSD License, see ../docs/license.html
+  
+  Persistence of stlplus infinite integer type - inf
+
+------------------------------------------------------------------------------*/
+#include "persistent_contexts.hpp"
+#include "inf.hpp"
+
+namespace stlplus
+{
+
+  void dump_inf(dump_context&, const inf& data) throw(persistent_dump_failed);
+  void restore_inf(restore_context&, inf& data) throw(persistent_restore_failed);
+
+} // end namespace stlplus
+
+#endif
