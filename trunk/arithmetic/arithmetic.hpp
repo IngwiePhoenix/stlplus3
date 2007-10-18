80450f9c166b2e6619e9227a3821f1e0a4bb118a
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2007-10-18 17:30:09 +0000

    Header for including the whole library in one go
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@38 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

---
+++ trunk/arithmetic/arithmetic.hpp
@@ -0,0 +1,19 @@
+#ifndef STLPLUS_ARITHMETIC_HPP
+#define STLPLUS_ARITHMETIC_HPP
+/*------------------------------------------------------------------------------
+
+Author:    Andy Rushton
+Copyright: (c) Andy Rushton, 2007
+License:   BSD License, see ../docs/license.html
+
+Header for including the whole arithmetic library in one go
+
+------------------------------------------------------------------------------*/
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "inf.hpp"
+#include "bitwise_arithmetic.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
