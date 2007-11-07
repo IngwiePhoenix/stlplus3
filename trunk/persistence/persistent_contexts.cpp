01fd02da806edda869c6390e19d29f600378321a
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2007-11-07 18:50:35 +0000

    Tidy up and rearrange
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@53 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

--- trunk/persistence/persistent_contexts.cpp
+++ trunk/persistence/persistent_contexts.cpp
@@ -1,51 +1,54 @@
 /*------------------------------------------------------------------------------
 
-  Author:    Andy Rushton
-  Copyright: (c) Andy Rushton, 2007
-  License:   BSD License, see ../docs/license.html
+Author:    Andy Rushton
+Copyright: (c) Andy Rushton, 2007
+License:   BSD License, see ../docs/license.html
 
-  ------------------------------------------------------------------------------*/
+------------------------------------------------------------------------------*/
 #include "persistent_contexts.hpp"
 #include "persistent.hpp"
 #include <map>
 #include <string>
 
-////////////////////////////////////////////////////////////////////////////////
-// File format version
-
-// This relates to the layout of basic types - if I change the file layout of,
-// say, int or vector, then this will change
-
-// Early versions of the persistence routines did not have this - they are no longer supported
-// - Change from version 1 to 2: changed the persistent representation of inf
-
-unsigned char stlplus::PersistentVersion = 2;
-
-////////////////////////////////////////////////////////////////////////////////
-
-static std::string to_string(int number)
-{
-  // use sprintf in a very controlled way that cannot overrun
-  char* buffer = new char[50];
-  sprintf(buffer, "%i", number);
-  std::string result = buffer;
-  delete buffer;
-  return result;
-}
-
-static bool little_endian(void)
-{
-  int sample = 1;
-  char* sample_bytes = (char*)&sample;
-  return sample_bytes[0] != 0;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// dump context classes
-////////////////////////////////////////////////////////////////////////////////
-
 namespace stlplus
 {
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // File format version
+
+  // This relates to the layout of basic types - if I change the file layout of,
+  // say, int or vector, then this will change
+
+  // Early versions of the persistence routines did not have this - they are no longer supported
+  // - Change from version 1 to 2: changed the persistent representation of inf
+
+  unsigned char PersistentVersion = 2;
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // avoid creating dependencies on other libraries
+
+  static std::string to_string(int number)
+  {
+    // use sprintf in a very controlled way that cannot overrun
+    char* buffer = new char[50];
+    sprintf(buffer, "%i", number);
+    std::string result = buffer;
+    delete buffer;
+    return result;
+  }
+
+  static bool little_endian(void)
+  {
+    // TODO - find a compile-time way of doing this
+    int sample = 1;
+    char* sample_bytes = (char*)&sample;
+    return sample_bytes[0] != 0;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // dump context classes
+  ////////////////////////////////////////////////////////////////////////////////
 
   class dump_context_body
   {
@@ -63,8 +66,9 @@ namespace stlplus
     interface_map m_interfaces;
 
     dump_context_body(std::ostream& device, unsigned char version) throw(persistent_dump_failed) : 
-      m_max_key(0), m_version(version), m_little_endian(::little_endian()), m_device(&device)
+      m_max_key(0), m_version(version), m_little_endian(stlplus::little_endian()), m_device(&device)
       {
+        // write the version number as a single byte
         put(version);
         // map a null pointer onto magic number zero
         m_pointers[0] = 0;
@@ -153,86 +157,81 @@ namespace stlplus
       }
   };
 
-} // end namespace stlplus
-
   ////////////////////////////////////////////////////////////////////////////////
 
-stlplus::dump_context::dump_context(std::ostream& device, unsigned char version) throw(stlplus::persistent_dump_failed) : m_body(0)
-{
-  m_body = new stlplus::dump_context_body(device,version);
-}
+  dump_context::dump_context(std::ostream& device, unsigned char version) throw(persistent_dump_failed) : m_body(0)
+  {
+    m_body = new dump_context_body(device,version);
+  }
 
-stlplus::dump_context::~dump_context(void)
-{
-  delete m_body;
-}
+  dump_context::~dump_context(void)
+  {
+    delete m_body;
+  }
 
-void stlplus::dump_context::put(unsigned char data) throw(persistent_dump_failed)
-{
-  m_body->put(data);
-}
+  void dump_context::put(unsigned char data) throw(persistent_dump_failed)
+  {
+    m_body->put(data);
+  }
 
-const std::ostream& stlplus::dump_context::device(void) const
-{
-  return m_body->device();
-}
+  const std::ostream& dump_context::device(void) const
+  {
+    return m_body->device();
+  }
 
-unsigned char stlplus::dump_context::version(void) const
-{
-  return m_body->version();
-}
+  unsigned char dump_context::version(void) const
+  {
+    return m_body->version();
+  }
 
-bool stlplus::dump_context::little_endian(void) const
-{
-  return m_body->little_endian();
-}
+  bool dump_context::little_endian(void) const
+  {
+    return m_body->little_endian();
+  }
 
-std::pair<bool,unsigned> stlplus::dump_context::pointer_map(const void* const pointer)
-{
-  return m_body->pointer_map(pointer);
-}
+  std::pair<bool,unsigned> dump_context::pointer_map(const void* const pointer)
+  {
+    return m_body->pointer_map(pointer);
+  }
 
-unsigned stlplus::dump_context::register_callback(const std::type_info& info, stlplus::dump_context::dump_callback callback)
-{
-  return m_body->register_callback(info,callback);
-}
+  unsigned dump_context::register_callback(const std::type_info& info, dump_context::dump_callback callback)
+  {
+    return m_body->register_callback(info,callback);
+  }
 
-bool stlplus::dump_context::is_callback(const std::type_info& info) const
-{
-  return m_body->is_callback(info);
-}
+  bool dump_context::is_callback(const std::type_info& info) const
+  {
+    return m_body->is_callback(info);
+  }
 
-stlplus::dump_context::callback_data stlplus::dump_context::lookup_callback(const std::type_info& info) const throw(persistent_illegal_type)
-{
-  return m_body->lookup_callback(info);
-}
+  dump_context::callback_data dump_context::lookup_callback(const std::type_info& info) const throw(persistent_illegal_type)
+  {
+    return m_body->lookup_callback(info);
+  }
 
-unsigned stlplus::dump_context::register_interface(const std::type_info& info)
-{
-  return m_body->register_interface(info);
-}
+  unsigned dump_context::register_interface(const std::type_info& info)
+  {
+    return m_body->register_interface(info);
+  }
 
-bool stlplus::dump_context::is_interface(const std::type_info& info) const
-{
-  return m_body->is_interface(info);
-}
+  bool dump_context::is_interface(const std::type_info& info) const
+  {
+    return m_body->is_interface(info);
+  }
 
-unsigned stlplus::dump_context::lookup_interface(const std::type_info& info) const throw(persistent_illegal_type)
-{
-  return m_body->lookup_interface(info);
-}
+  unsigned dump_context::lookup_interface(const std::type_info& info) const throw(persistent_illegal_type)
+  {
+    return m_body->lookup_interface(info);
+  }
 
-void stlplus::dump_context::register_all(stlplus::dump_context::installer installer)
-{
-  if (installer) installer(*this);
-}
+  void dump_context::register_all(dump_context::installer installer)
+  {
+    if (installer) installer(*this);
+  }
 
-////////////////////////////////////////////////////////////////////////////////
-// restore context classes
-////////////////////////////////////////////////////////////////////////////////
-
-namespace stlplus
-{
+  ////////////////////////////////////////////////////////////////////////////////
+  // restore context classes
+  ////////////////////////////////////////////////////////////////////////////////
 
   class restore_context_body
   {
@@ -251,7 +250,7 @@ namespace stlplus
     interface_map m_interfaces;
 
     restore_context_body(std::istream& device) throw(persistent_restore_failed) : 
-      m_max_key(0), m_little_endian(::little_endian()), m_device(&device)
+      m_max_key(0), m_little_endian(stlplus::little_endian()), m_device(&device)
       {
         // map a null pointer onto magic number zero
         m_pointers[0] = 0;
@@ -349,84 +348,85 @@ namespace stlplus
       }
   };
 
-} // end namespace stlplus
+  ////////////////////////////////////////////////////////////////////////////////
+
+  restore_context::restore_context(std::istream& device) throw(persistent_restore_failed) : 
+    m_body(0)
+  {
+    m_body = new restore_context_body(device);
+  }
+
+  restore_context::~restore_context(void)
+  {
+    delete m_body;
+  }
+
+  const std::istream& restore_context::device(void) const
+  {
+    return m_body->device();
+  }
+
+  unsigned char restore_context::version(void) const
+  {
+    return m_body->version();
+  }
+
+  bool restore_context::little_endian(void) const
+  {
+    return m_body->little_endian();
+  }
+
+  int restore_context::get(void) throw(persistent_restore_failed)
+  {
+    return m_body->get();
+  }
+
+  std::pair<bool,void*> restore_context::pointer_map(unsigned magic)
+  {
+    return m_body->pointer_map(magic);
+  }
+
+  void restore_context::pointer_add(unsigned magic, void* new_pointer)
+  {
+    m_body->pointer_add(magic,new_pointer);
+  }
+
+  unsigned restore_context::register_callback(restore_context::create_callback create, restore_context::restore_callback restore)
+  {
+    return m_body->register_callback(create,restore);
+  }
+
+  bool restore_context::is_callback(unsigned key) const
+  {
+    return m_body->is_callback(key);
+  }
+
+  restore_context::callback_data restore_context::lookup_callback(unsigned key) const throw(persistent_illegal_type)
+  {
+    return m_body->lookup_callback(key);
+  }
+
+  unsigned restore_context::register_interface(persistent* sample)
+  {
+    return m_body->register_interface(sample);
+  }
+
+  bool restore_context::is_interface(unsigned key) const
+  {
+    return m_body->is_interface(key);
+  }
+
+  persistent* restore_context::lookup_interface(unsigned key) const throw(persistent_illegal_type)
+  {
+    return m_body->lookup_interface(key);
+  }
+
+  void restore_context::register_all(restore_context::installer installer)
+  {
+    if (installer) installer(*this);
+  }
 
   ////////////////////////////////////////////////////////////////////////////////
 
-stlplus::restore_context::restore_context(std::istream& device) throw(stlplus::persistent_restore_failed) : 
-  m_body(0)
-{
-  m_body = new stlplus::restore_context_body(device);
-}
+} // end namespace stlplus
 
-stlplus::restore_context::~restore_context(void)
-{
-  delete m_body;
-}
-
-const std::istream& stlplus::restore_context::device(void) const
-{
-  return m_body->device();
-}
-
-unsigned char stlplus::restore_context::version(void) const
-{
-  return m_body->version();
-}
-
-bool stlplus::restore_context::little_endian(void) const
-{
-  return m_body->little_endian();
-}
-
-int stlplus::restore_context::get(void) throw(stlplus::persistent_restore_failed)
-{
-  return m_body->get();
-}
-
-std::pair<bool,void*> stlplus::restore_context::pointer_map(unsigned magic)
-{
-  return m_body->pointer_map(magic);
-}
-
-void stlplus::restore_context::pointer_add(unsigned magic, void* new_pointer)
-{
-  m_body->pointer_add(magic,new_pointer);
-}
-
-unsigned stlplus::restore_context::register_callback(stlplus::restore_context::create_callback create, stlplus::restore_context::restore_callback restore)
-{
-  return m_body->register_callback(create,restore);
-}
-
-bool stlplus::restore_context::is_callback(unsigned key) const
-{
-  return m_body->is_callback(key);
-}
-
-stlplus::restore_context::callback_data stlplus::restore_context::lookup_callback(unsigned key) const throw(stlplus::persistent_illegal_type)
-{
-  return m_body->lookup_callback(key);
-}
-
-unsigned stlplus::restore_context::register_interface(stlplus::persistent* sample)
-{
-  return m_body->register_interface(sample);
-}
-
-bool stlplus::restore_context::is_interface(unsigned key) const
-{
-  return m_body->is_interface(key);
-}
-
-stlplus::persistent* stlplus::restore_context::lookup_interface(unsigned key) const throw(stlplus::persistent_illegal_type)
-{
-  return m_body->lookup_interface(key);
-}
-
-void stlplus::restore_context::register_all(stlplus::restore_context::installer installer)
-{
-  if (installer) installer(*this);
-}
-
-////////////////////////////////////////////////////////////////////////////////
