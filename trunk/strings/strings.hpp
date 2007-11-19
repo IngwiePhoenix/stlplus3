345b2aa898dc307eee4dfdb1c3f3b4466bc06a96
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2007-11-19 18:45:18 +0000

    Tidy up and re-style banner
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@71 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

--- trunk/strings/strings.hpp
+++ trunk/strings/strings.hpp
@@ -1,23 +1,19 @@
 #ifndef STLPLUS_STRINGS
 #define STLPLUS_STRINGS
-/*------------------------------------------------------------------------------
-
-  Author:    Andy Rushton
-  Copyright: (c) Andy Rushton, 2007
-  License:   BSD License, see ../docs/license.html
-
-  Header for including the whole strings library in one go
-
-  The strings library prints C and C++ types into a std::string
-
-  - it extends the numeric presentation of integers to include any radix from 2 - 36
-  - it allows the printing of data structures - useful for diagnostic dumps
-
-  ------------------------------------------------------------------------------*/
-
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "format_types.hpp"
+//   Author:    Andy Rushton
+//   Copyright: (c) Andy Rushton, 2007
+//   License:   BSD License, see ../docs/license.html
+
+//   Header for including the whole strings library in one go
+
+//   The strings library prints C and C++ types into a std::string
+
+//   - it extends the numeric presentation of integers to include any radix from 2 - 36
+//   - it allows the printing of data structures - useful for diagnostic dumps
+
+////////////////////////////////////////////////////////////////////////////////
 
 #include "string_utilities.hpp"
 
