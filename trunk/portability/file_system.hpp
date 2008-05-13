43d65cd52dc60fe5e0086cd9440c844fd0021d04
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2008-05-13 17:26:33 +0000

    removed mode handling which was broken on Windows, improved header comments
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@137 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

--- trunk/portability/file_system.hpp
+++ trunk/portability/file_system.hpp
@@ -30,95 +30,107 @@ namespace stlplus
   ////////////////////////////////////////////////////////////////////////////////
   // classifying functions
 
+  // test for whether there's something (i.e. folder or file) with this name
   bool is_present(const std::string& thing);
+  // test for whether there's something present and its a folder
   bool is_folder(const std::string& thing);
+  // test for whether there's something present and its a file
   bool is_file(const std::string& thing);
 
   ////////////////////////////////////////////////////////////////////////////////
   // file functions
 
+  // tests whether there's a file of this name
   bool file_exists(const std::string& filespec);
+  // tests whether the file is readable - i.e. exists and has read mode set
   bool file_readable(const std::string& filespec);
+  // tests whether file is writable - either it exists and is writable or doesn't exist but is in a writable folder
   bool file_writable(const std::string& filespec);
+  // the size of the file in bytes - 0 if doesn't exist
   size_t file_size(const std::string& filespec);
+  // delete the file - returns true if the delete succeeded
   bool file_delete(const std::string& filespec);
+  // rename the file - returns true if the rename succeeded
   bool file_rename (const std::string& old_filespec, const std::string& new_filespec);
-  bool file_move (const std::string& old_filespec, const std::string& new_filespec);
+  // make an exact copy of the file - returns true if it succeeded
   bool file_copy (const std::string& old_filespec, const std::string& new_filespec);
-
-  // Read-only versus read-write control. This is equivalent to chmod on Unix,
-  // but I've insulated the user from the low-level routine because of
-  // differences in the OSs' interpretation of the mode parameter. I've also
-  // defined a new set of constants to control this, again because of
-  // inconsistencies. The idea is to combine the constants as bit-masks so as to
-  // build up a set of permissions. The modes are ORed together to build up a
-  // set of permissions and then ANDed with a mask to control which people have
-  // that permission. Permissions can be ORed together too. So, for example, to
-  // give the owner read-write access and all others only read access, you would
-  // use the expression:
-  //   ((read_mode | write_mode) & owner_mask) | (read_mode & (group_mask | other_mask))
-  // This can be simplified by using combined modes and combined masks to:
-  //   (read_write_mode & owner_mask) | (read_mode & non_owner_mask)
-
-  // basic modes
-  extern const int read_mode;
-  extern const int write_mode;
-  extern const int execute_mode;
-  // combined modes
-  extern const int none_mode;
-  extern const int read_write_mode;
-  extern const int all_mode;
-  // basic users
-  extern const int owner_mask;
-  extern const int group_mask;
-  extern const int other_mask;
-  // combined users
-  extern const int non_owner_mask;
-  extern const int all_mask;
-  // common settings
-  extern const int read_mode_all;
-  extern const int read_write_mode_owner_read_mode_all;
-  extern const int read_mode_owner_only;
-  extern const int read_write_mode_owner_only;
-  // the function itself
-  bool file_set_mode(const std::string& filespec, int mode);
+  // move the file - tries to rename, if that fails, tries to copy - returns true if either of these succeeded
+  bool file_move (const std::string& old_filespec, const std::string& new_filespec);
 
   // get the file's time stamps as a time_t - see the stlplus time.hpp package
+
+  // time the file was originally created
   time_t file_created(const std::string& filespec);
+  // time the file was last modified
   time_t file_modified(const std::string& filespec);
+  // time the file was accessed
   time_t file_accessed(const std::string& filespec);
 
   // platform-specific string handling to combine a directory and filename into a path
+
+  // combine a folder with a filename (basename.extension)
   std::string create_filespec(const std::string& folder, const std::string& filename);
+  // combine a folder, a basename and an extension - extension does not need the .
   std::string create_filespec(const std::string& folder, const std::string& basename, const std::string& extension);
+  // combine a basename and an extension - extension does not need the .
   std::string create_filename(const std::string& basename, const std::string& extension);
 
   ////////////////////////////////////////////////////////////////////////////////
   // folder functions
 
+  // craete a folder - returns true if successful
   bool folder_create(const std::string& folder);
+  // tests for whether the folder exists, i.e. there is something of that name and its a folder
   bool folder_exists(const std::string& folder);
+  // test whether the folder contents are readable
   bool folder_readable(const std::string& folder);
+  // tests whether the folder can be written to - for example to create a new file
   bool folder_writable(const std::string& folder);
+  // delete the folder, optionally deleting the contents first - only succeeds if everything could be deleted
   bool folder_delete(const std::string& folder, bool recurse = false);
+  // rename the folder - this probably only works within a disk/partition
   bool folder_rename (const std::string& old_directory, const std::string& new_directory);
+  // test whether the folder is empty (of files)
   bool folder_empty(const std::string& folder);
 
+  // set the current folder
   bool folder_set_current(const std::string& folder);
+
+  // platform-specific string handling to retrieve some special locations
+  // these do not check whether the folder exists, they just process strings
+
+  // get the current folder
   std::string folder_current(void);
+  // get the current folder as a full path
   std::string folder_current_full(void);
+  // get the home folder - $HOME or %HOMEDRIVE%%HOMEPATH%
   std::string folder_home(void);
+  // go down a level in the folder hierarchy
   std::string folder_down(const std::string& folder, const std::string& subfolder);
+  // go up a level in the folder hierarchy
   std::string folder_up(const std::string& folder, unsigned levels = 1);
 
+  // get folder contents
+
+  // the set of all subdirectories
   std::vector<std::string> folder_subdirectories(const std::string& folder);
+  // the set of all files
   std::vector<std::string> folder_files(const std::string& folder);
+  // the set of all folders and files
   std::vector<std::string> folder_all(const std::string& folder);
-  std::vector<std::string> folder_wildcard(const std::string& folder, const std::string& wildcard, bool subdirs = true, bool files = true);
+  // the set of all folder contents matching a wildcard string
+  // if folders is true, include folders; if files is true, include files
+  std::vector<std::string> folder_wildcard(const std::string& folder,
+                                           const std::string& wildcard,
+                                           bool folders = true,
+                                           bool files = true);
 
   ////////////////////////////////////////////////////////////////////////////////
   // path functions
 
+  // string manipulations of paths
+
+  // test whether a string represents a full path or a relative one
   bool is_full_path(const std::string& path);
   bool is_relative_path(const std::string& path);
 
@@ -144,9 +156,13 @@ namespace stlplus
   ////////////////////////////////////////////////////////////////////////////////
   // access functions split a filespec into its elements
 
+  // get the basename - that is, the name of the file without folder or extension parts
   std::string basename_part(const std::string& filespec);
+  // get the filename - that is, the name of the file without folder part but with extension
   std::string filename_part(const std::string& filespec);
+  // get the extension - that is the part of the filename after the . (and excluding the .)
   std::string extension_part(const std::string& filespec);
+  // get the folder part - that is the filespec with the filename removed
   std::string folder_part(const std::string& filespec);
 
   // split a path into a vector of elements - i.e. split at the folder separator
