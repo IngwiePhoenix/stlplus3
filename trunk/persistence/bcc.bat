3459c978e3556db60d3ef026cd35e839105431c9
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2009-10-02 12:11:00 +0000

    Minor change to Borland script
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@312 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

--- trunk/persistence/bcc.bat
+++ trunk/persistence/bcc.bat
@@ -8,5 +8,5 @@ bcc32 -I..\portability;..\containers -c +..\bcc.cfg -oBORLAND-i686-debug\persist
 bcc32 -I..\portability;..\containers -c +..\bcc.cfg -oBORLAND-i686-debug\persistent_int.obj persistent_int.cpp
 bcc32 -I..\portability;..\containers -c +..\bcc.cfg -oBORLAND-i686-debug\persistent_string.obj persistent_string.cpp
 bcc32 -I..\portability;..\containers -c +..\bcc.cfg -oBORLAND-i686-debug\persistent_vector.obj persistent_vector.cpp
-del "BORLAND-i686-debug\persistence.lib"
+del /Q "BORLAND-i686-debug\persistence.lib"
 tlib "BORLAND-i686-debug\persistence.lib" /a "BORLAND-i686-debug\persistent_bool.obj" "BORLAND-i686-debug\persistent_contexts.obj" "BORLAND-i686-debug\persistent_cstring.obj" "BORLAND-i686-debug\persistent_exceptions.obj" "BORLAND-i686-debug\persistent_float.obj" "BORLAND-i686-debug\persistent_inf.obj" "BORLAND-i686-debug\persistent_int.obj" "BORLAND-i686-debug\persistent_string.obj" "BORLAND-i686-debug\persistent_vector.obj" 
