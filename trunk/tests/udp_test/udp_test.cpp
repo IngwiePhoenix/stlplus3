0b964414729336540769d68e1de401f1dbf8a504
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2009-10-02 09:17:48 +0000

    Newly written test for TCP, then adapted to test UDP
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@302 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

--- trunk/tests/udp_test/udp_test.cpp
+++ trunk/tests/udp_test/udp_test.cpp
@@ -1,5 +1,5 @@
 #include "build.hpp"
-#include "tcp_sockets.hpp"
+#include "udp_sockets.hpp"
 #include "string_utilities.hpp"
 #include "subprocesses.hpp"
 #include <vector>
@@ -7,16 +7,16 @@
 #include <iostream>
 
 ////////////////////////////////////////////////////////////////////////////////
-// TCP test: this program is designed to be run twice - once without an
+// UDP test: this program is designed to be run twice - once without an
 // argument as the server, which itself runs a second copy with an argument as
-// the client. The two instances then swap information using TCP
+// the client. The two instances then swap information using UDP
 ////////////////////////////////////////////////////////////////////////////////
 
 unsigned timeout = 10000000;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Server code
-// sets up a TCP server then spawns this program again in client mode
+// sets up a UDP server then spawns this program again in client mode
 
 int server(std::vector<std::string> values, int port, const std::string& command)
 {
@@ -25,96 +25,76 @@ int server(std::vector<std::string> values, int port, const std::string& command
   // this is the first-run instance and acts as the server
   // setup a server on the shared port
   std::cerr << "server: started" << std::endl;
-  stlplus::TCP_server server(port);
-  std::cerr << "server: set up TCP server on port " << port << std::endl;
+  stlplus::UDP_server connection(port);
+  std::cerr << "server: set up UDP server on port " << port << std::endl;
   // now spawn a second copy of this program
   stlplus::async_subprocess client;
   client.spawn(command);
   std::cerr << "server: spawned client with command: " << command << std::endl;
-  // now wait for a client connection to come in
+  // UDP servers just respond immediately to incoming datagrams
   std::cerr << "server: waiting for incoming connection" << std::endl;
-  if (!server.accept_ready(timeout))
+  for (unsigned request = 0; request < values.size(); request++)
   {
-    std::cerr << "server: ERROR: wait for incoming connection timed out" << std::endl;
-    ++errors;
-  }
-  else
-  {
-    std::cerr << "server: accepting incoming connection" << std::endl;
-    stlplus::TCP_connection connection = server.accept();
-    unsigned request = 0;
-    while(connection.initialised())
+    std::cerr << "server: checking for receive ready" << std::endl;
+    // check connection for incoming request
+    if (!connection.receive_ready(timeout))
     {
-      std::cerr << "server: checking for receive request" << std::endl;
-      // check connection for incoming request
-      if (!connection.receive_ready(timeout))
+      std::cerr << "server: ERROR: wait for receive timed out" << std::endl;
+      ++errors;
+    }
+    else
+    {
+      // receive the request
+      std::string data;
+      unsigned long remote_address = 0;
+      unsigned short remote_port = 0;
+      if (!connection.receive(data, remote_address, remote_port))
       {
-        std::cerr << "server: ERROR: wait for receive timed out" << std::endl;
+        std::cerr << "server: ERROR: connection receive failed after receive_ready was true" << std::endl;
         ++errors;
       }
       else
       {
-        // receive the request
-        std::string data;
-        if (!connection.receive(data))
+        // report the value and check it's correct
+        std::cerr << "server: received \"" << data << "\" from " << remote_address << ":" << remote_port << std::endl;
+        std::string value = std::string("error");
+        if (request < values.size())
+          value = values[request];
+        if (data != value)
         {
-          std::cerr << "server: ERROR: connection receive failed after receive_ready was true" << std::endl;
+          std::cerr << "server: ERROR: received data \"" << data << "\" != \"" << value << "\"" << std::endl;
           ++errors;
         }
-        else if (data.size() == 0)
+        // now throw the same data back
+        std::cerr << "server: testing for send ready" << std::endl;
+        if (!connection.send_ready(timeout))
         {
-          // legitimate (?) last receive of an empty string
-          std::cerr << "server: received empty string \"" << data << "\"" << std::endl;
+          std::cerr << "server: ERROR: wait for send timed out" << std::endl;
+          ++errors;
         }
         else
         {
-          // report the value and check it's correct
-          std::cerr << "server: received \"" << data << "\"" << std::endl;
-          std::string value = std::string("error");
-          if (request < values.size())
-            value = values[request];
-          if (data != value)
+          std::cerr << "server: sending \"" << data << "\"" << std::endl;
+          if (!connection.send(data, remote_address, remote_port))
           {
-            std::cerr << "server: ERROR: received data \"" << data << "\" != \"" << value << "\"" << std::endl;
+            std::cerr << "server: ERROR: connection send failed after send_ready was true" << std::endl;
             ++errors;
           }
-          // now throw the same data back
-          if (!connection.send_ready(timeout))
-          {
-            std::cerr << "server: ERROR: wait for send timed out" << std::endl;
-            ++errors;
-          }
-          else
-          {
-            std::cerr << "server: sending \"" << data << "\"" << std::endl;
-            if (!connection.send(data))
-            {
-              std::cerr << "server: ERROR: connection send failed after send_ready was true" << std::endl;
-              ++errors;
-            }
-          }
-          request++;
-        }            
-      }
-      // just check for client having exited
-      if (!client.tick())
-      {
-        std::cerr << "server: client has exited - closing connection" << std::endl;
-        connection.close();
-      }
+        }
+      }            
     }
-    std::cerr << "server: exited server loop" << std::endl;
-    // wait for client to exit
-    while (client.tick())
-    {
-      std::cerr << "server: client has not exited - waiting" << std::endl;
-      sleep(1);
-    }
-    if (client.exit_status() != 0)
-    {
-      std::cerr << "server: ERROR: client has exited with status " << client.exit_status() << std::endl;
-      errors += client.exit_status();
-    }
+  }
+  std::cerr << "server: exited server loop" << std::endl;
+  // wait for client to exit
+  while (client.tick())
+  {
+    std::cerr << "server: client has not exited - waiting" << std::endl;
+    sleep(1);
+  }
+  if (client.exit_status() != 0)
+  {
+    std::cerr << "server: ERROR: client has exited with status " << client.exit_status() << std::endl;
+    errors += client.exit_status();
   }
   std::cerr << "server: " << (errors ? "ERROR:" : "SUCCESS:") << " exiting with " << errors << " errors" << std::endl;
   return errors;
@@ -129,13 +109,12 @@ int client(std::vector<std::string> values, int port)
   // this is the second-run instance and acts as the client
   std::cerr << "client: " << stlplus::build() << std::endl;
   std::cerr << "client: creating connection" << std::endl;
-  stlplus::TCP_client connection("localhost", port, timeout);
-  unsigned request = 0;
-  while(connection.connected())
+  stlplus::UDP_client connection("localhost", port, timeout);
+  for(unsigned request = 0; request < values.size(); request++)
   {
     std::string value = values[request];
     // send this value
-    std::cerr << "client: sending data" << std::endl;
+    std::cerr << "client: testing for send ready" << std::endl;
     if (!connection.send_ready(timeout))
     {
       std::cerr << "client: ERROR: wait for send timed out" << std::endl;
@@ -153,6 +132,7 @@ int client(std::vector<std::string> values, int port)
       }
     }
     // check connection for incoming request
+    std::cerr << "client: testing for receive ready" << std::endl;
     if (!connection.receive_ready(timeout))
     {
       std::cerr << "client: ERROR: wait for receive timed out" << std::endl;
@@ -176,13 +156,7 @@ int client(std::vector<std::string> values, int port)
           std::cerr << "client: ERROR: received data \"" << data << "\" != \"" << value << "\"" << std::endl;
           ++errors;
         }
-        request++;
       }            
-    }
-    if (request >= values.size())
-    {
-      std::cerr << "client: all values sent, closing connection" << std::endl;
-      connection.close();
     }
   }
   std::cerr << "client: exited communication loop" << std::endl;
