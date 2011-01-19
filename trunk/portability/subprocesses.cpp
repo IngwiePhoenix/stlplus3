17b40a7d97e63ca109b5183853125c10d5a565ce
andy_rushton <andy_rushton@ebc0ae9a-4168-4a02-ab27-d0bb227af65f>
2011-01-19 00:19:44 +0000

    Compatibility problem with Borland compiler
    git-svn-id: https://svn.code.sf.net/p/stlplus/stlplus3@437 ebc0ae9a-4168-4a02-ab27-d0bb227af65f

--- trunk/portability/subprocesses.cpp
+++ trunk/portability/subprocesses.cpp
@@ -22,6 +22,10 @@
 #include <stdlib.h>
 
 #ifdef MSWINDOWS
+#ifdef __BORLANDC__
+// missing declaration in Borland headers
+LPTCH WINAPI GetEnvironmentStringsA(void);
+#endif
 #else
 extern char** environ;
 #include <signal.h>
@@ -490,15 +494,10 @@ namespace stlplus
 #endif
   ////////////////////////////////////////////////////////////////////////////////
 
-  #ifdef __BORLANDC__
-  // missing declaration in Borland headers
-  LPTCH WINAPI GetEnvironmentStringsA(void);
-  #endif
-
   env_vector::env_vector(void)
   {
 #ifdef MSWINDOWS
-	char* env = (char*)GetEnvironmentStringsA();
+    char* env = (char*)GetEnvironmentStringsA();
     m_env = envp_copy(env);
     FreeEnvironmentStringsA(env);
 #else
